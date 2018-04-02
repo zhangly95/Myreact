70157dbfa73286609780c868a6e045efa595aa0e
zhangly95 <zhangliyuan@sinap.ac.cn>
2018-04-02 18:52:17 +0800

    表单提交功能

---
+++ Library/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Home extends React.Component {
+  render () {
+    return (
+      <div>
+        <header>
+          <h1>Welcome</h1>
+        </header>
+
+        <main>
+        <Link to="/user/list">用户列表</Link>
+        <br/>
+          <Link to="/user/add">添加用户</Link>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Home;
